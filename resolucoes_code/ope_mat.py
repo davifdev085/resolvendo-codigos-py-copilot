0df8e79dcf89a11e6b2df922b42bdac4c1762db7
Davi Felipe | Developer | Tech <davifelipedev@gmail.com>
2024-06-24 12:58:33 -0300

    Update ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Solicitando os números como entrada do usuário
+numero1 = float(input("Digite o primeiro número: "))  
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+
+print("A soma de", numero1, "e", numero2, "é:", soma)
