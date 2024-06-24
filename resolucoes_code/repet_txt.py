34ce6aa623d5014c79fac4839b5da967f4c3c94c
Davi Felipe | Developer | Tech <davifelipedev@gmail.com>
2024-06-24 13:00:31 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,7 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+numero1 = float(input("Digite um numero:"))
+numero2 = float(input("DIgite outro numero:"))
+
+soma = numero1 + numero2
+print(soma)
